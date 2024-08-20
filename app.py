74e4143b6d1708399da79167f89d0848c80cbff9
Slautin <bslautin@gmail.com>
2024-08-20 15:03:35 +0200

    server v1

--- app.py
+++ app.py
@@ -5,94 +5,108 @@ import numpy as np
 app = Flask(__name__)
 
 # Global variables to track readiness, data exchange, and communication state
-notebook1_ready = False
-notebook2_ready = False
-received_array = None
-notebook1_message = None
-notebook2_message = None
+microscope_ready = False
+notebook_ready = False
+coordinates = None
+location = None
+response = None
 channel_open = True
 
 
-@app.route('/notebook1_ready', methods=['POST'])
-def notebook1_ready_status():
-    global notebook1_ready
-    notebook1_ready = True
-    return jsonify({"status": "Notebook 1 ready", "notebook2_ready": notebook2_ready})
+@app.route('/microscope_ready', methods=['POST'])
+def microscope_ready():
+    global microscope_ready
+    microscope_ready = True
+    return jsonify({"microscope": microscope_ready, "notebook": notebook_ready})
 
 
-@app.route('/notebook2_ready', methods=['POST'])
-def notebook2_ready_status():
-    global notebook2_ready
-    notebook2_ready = True
-    return jsonify({"status": "Notebook 2 ready", "notebook1_ready": notebook1_ready})
+@app.route('/notebook_ready', methods=['POST'])
+def notebook_ready():
+    global notebook_ready
+    notebook_ready = True
+    return jsonify({"microscope": microscope_ready, "notebook": notebook_ready})
+
+@app.route('/microscope_ready', methods=['POST'])
+def microscope_stop():
+    global microscope_ready
+    microscope_ready = False
+    return jsonify({"microscope": microscope_ready, "notebook": notebook_ready})
+
+
+@app.route('/notebook_ready', methods=['POST'])
+def notebook_stop():
+    global notebook_ready
+    notebook_ready = False
+    return jsonify({"microscope": microscope_ready, "notebook": notebook_ready})
 
 
 @app.route('/status', methods=['GET'])
 def check_status():
-    return jsonify({"notebook1_ready": notebook1_ready, "notebook2_ready": notebook2_ready})
+    return jsonify({"microscope": microscope_ready, "notebook": notebook_ready})
 
-
-@app.route('/send_array', methods=['POST'])
+@app.route('/send_coordinates', methods=['POST'])
 def receive_array():
-    global received_array
+    global coordinates, notebook_ready
     data = request.get_json()
-    received_array = np.array(data["array"])  # Convert list to NumPy array
-    return jsonify({"status": "Array received", "received_array_shape": received_array.shape})
+    coordinates = np.array(data["coordinates"])  # Convert list to NumPy array
+    notebook_ready = False
+    return jsonify({"status": "Coordinates uploaded", "received_array_shape": coordinates.shape})
 
+@app.route('/get_coordinates', methods=['POST'])
+def send_coordinates():
+    global notebook_ready
+    if coordinates is not None:
+        coord = coordinates
+        notebook_ready = True
+        return jsonify({"status": "Coordinates sent", "coordinates": coord})
 
-@app.route('/check_array', methods=['GET'])
-def check_array():
-    if received_array is not None:
-        return jsonify({"status": "Array exists", "shape": received_array.shape})
+@app.route('/check_coordinates', methods=['GET'])
+def check_coordinates():
+    if coordinates is not None:
+        return jsonify({"status": "Coordinates exists", "shape": coordinates.shape})
     else:
-        return jsonify({"status": "No array received"})
-
+        return jsonify({"status": "No coordinates received"})
 
 @app.route('/send_message', methods=['POST'])
 def receive_message():
-    global notebook1_message, notebook2_message, channel_open, notebook1_ready, notebook2_ready
+    global location, response, microscope_ready, notebook_ready
     data = request.get_json()
     sender = data.get("sender")
-    message = data.get("message")
+    message = data.get("data")
 
-    if sender == "notebook1":
-        notebook1_message = message
+    if sender == "microscope":
+        response = message
+        notebook_ready = False
         if message == 'END':
-            notebook1_ready = False
-    elif sender == "notebook2":
-        notebook2_message = message
+            microscope_ready = False
+    elif sender == "notebook":
+        location = message
+        microscope_ready = False
         if message == 'END':
-            notebook2_ready = False
-
-    if message == "END":
-        channel_open = False
-        return jsonify({"status": "Channel closed"})
+            notebook_ready = False
 
     return jsonify({"status": "Message received", "message": message})
 
 
 @app.route('/receive_message', methods=['GET'])
 def send_message():
-    global notebook1_message, notebook2_message
+    global location, response, microscope_ready, notebook_ready
     requester = request.args.get("requester")
 
-    if requester == "notebook1" and notebook2_message:
-        msg = notebook2_message
-        notebook2_message = None  # Clear the message after sending
-        return jsonify({"message": msg})
+    if requester == "microscope" and location:
+        msg = location
+        location = None  # Clear the message after sending
+        microscope_ready = True
+        return jsonify({"reponse": msg})
 
-    if requester == "notebook2" and notebook1_message:
-        msg = notebook1_message
-        notebook1_message = None  # Clear the message after sending
-        return jsonify({"message": msg})
+    if requester == "notebook" and response:
+        msg = response
+        response = None  # Clear the message after sending
+        notebook_ready = True
+        return jsonify({"location": msg})
 
     return jsonify({"message": None})
 
 
-@app.route('/check_channel', methods=['GET'])
-def check_channel():
-    return jsonify({"channel_open": channel_open})
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
